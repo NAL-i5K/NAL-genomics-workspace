63c6735aaaefc467b47d01c09344691da5241489
childers <genetics.guy@gmail.com>
2018-04-12 23:11:40 -0400

    commenting all references to load drupal

--- app/views.py
+++ app/views.py
@@ -20,8 +20,8 @@ from functools import wraps
 from .forms import InfoChangeForm, SetInstitutionForm, RegistrationForm
 from .models import Profile
 from social.apps.django_app.default.models import UserSocialAuth
-from i5k.settings import DRUPAL_URL, DRUPAL_COOKIE_DOMAIN
-from drupal_sso.models import DrupalUserMapping
+# from i5k.settings import DRUPAL_URL, DRUPAL_COOKIE_DOMAIN
+# from drupal_sso.models import DrupalUserMapping
 from django.contrib.auth.models import User
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
